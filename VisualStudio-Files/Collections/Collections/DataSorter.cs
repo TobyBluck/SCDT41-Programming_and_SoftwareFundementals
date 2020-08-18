61d9d072556331e0605c2f8fb2a9a8ee37dcd80c
TobyBluck <57405593+TobyBluck@users.noreply.github.com>
2020-08-18 17:16:59 +0100

    Adding Collections example

---
+++ VisualStudio-Files/Collections/Collections/DataSorter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class DataSorter
+    {
+
+        public class dataSorter:IComparer<Request>
+        {
+            public int Compare(Request R1, Request R2)
+            {
+                return R1.Date.CompareTo(R2.Date); 
+            }
+        }
+
+    }
+}
---
+++ VisualStudio-Files/Collections/Collections/request.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class Request
+    {
+        string requestor;
+        int priority;
+        DateTime date;
+
+        public string Requestor
+        { 
+            get { return requestor; }
+            set { requestor = value; }
+        }
+
+        public int Priority
+        {
+            get { return priority; }
+            set { priority = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public Request (string requestor, int priority, DateTime date)
+        {
+            this.Requestor = requestor;
+            this.Priority = priority;
+            this.Date = date; 
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}, {1}, {2}", this.Requestor, this.Priority, this.Date);
+        }
+    }
+}
