20b09bf85327ff722e14acd72e328273123d9457
TobyBluck <57405593+TobyBluck@users.noreply.github.com>
2020-08-18 17:29:34 +0100

    Adds two example programs we quickly made during class

---
+++ VisualStudio-Files/Program1-2/Program1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingWeek2_OutClass
+{
+    class Program
+    {
+
+        public class Person
+        {
+
+            public string Name; //name 
+            public int Age; //Age 
+
+            public Person()
+            {
+
+            }
+
+            public Person(string name)
+            {
+                this.Name = name; 
+            }
+
+            public Person (string name, int age)
+            {
+                this.Name = name;
+                this.Age = age; 
+            }
+
+        }
+
+        //------------------------------------------------------------------------
+
+        public class Staff : Person
+        {
+
+            public int StaffID;
+            public int StaffSalary;
+
+            public Staff()
+            {
+
+            }
+
+            public Staff(int staffID)
+            {
+                this.StaffID = staffID; 
+            }
+
+            public Staff(int staffID, int staffSalary)
+            {
+                this.StaffID = staffID;
+                this.StaffSalary = staffSalary;
+            }
+
+            public void ShowPass()
+            {
+                Console.WriteLine("My name is {0}", Name);
+                Console.WriteLine("My ID number is: " + StaffID);
+                Console.WriteLine("Salary is: " + StaffSalary);
+            }
+
+            //------------------------------------------------------------------------
+
+            public class Visitors : Person
+            {
+
+                public string WhoSee;
+                public int LenghtTime;
+
+                public Visitors()
+                {
+                
+                }
+
+                public Visitors(string whoSee)
+                {
+                    this.WhoSee = whoSee; 
+                }
+
+                public Visitors(string whoSee, int lenghtTime)
+                {
+                    this.WhoSee = whoSee;
+                    this.LenghtTime = lenghtTime;
+                }
+
+                public void ShowPass()
+                {
+                    Console.WriteLine("My name is {0}", Name);
+                    Console.WriteLine("Who to see is {1}", WhoSee);
+                    Console.WriteLine("Lenght of time(HRS) = " + LenghtTime);
+                   
+                }
+
+            }
+
+            //-------------------------------------------------------------------------
+
+            public class Clients : Person
+            {
+
+                // boolean for mainte or Delivery 
+
+                //GUID as a identification which prints as proof, use something like bleow which adds a time stamps. 
+                /*var ticks = DateTime.Now.Ticks;
+                var guid = Guid.NewGuid().ToString();
+                var uniqueSessionId = ticks.ToString() + '-' + guid; //guid created by combining ticks and guid
+
+                var datetime = new DateTime(ticks);//for checking purpose
+                var datetimenow = DateTime.Now;    //both these date times are different.*/
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+           /* Stopwatch timer = new Stopwatch();
+            timer.Start();
+            ---code here---
+            timer.Stop();
+            Console.WriteLine("Time elapsed: {0:hh\\:mm\\:ss}", stopwatch.Elapsed); */
+        }
+    }
+}
---
+++ VisualStudio-Files/Program1-2/Program2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenCentre
+{
+    public class Square
+    {
+        //lenght x width = area. 
+        public double Lenght;
+        public double Width;
+
+        public void Calculated() //calculate method 
+        {
+
+            double total; //stores total size 
+            double totalCost; //stores final cost 
+            total = Lenght* Width; //calaculate size
+            totalCost = total*2.10; //calculates size 
+            Console.WriteLine("total cosr is: £{0}", totalCost);
+        }
+    }   
+
+    
+
+
+    
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        var square = new Square();
+        Console.WriteLine("Input lenght");
+        square.Lenght = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Input width");
+        square.Width = Convert.ToDouble(Console.ReadLine());
+
+        square.Calculated(); 
+
+        }
+    }
+}
