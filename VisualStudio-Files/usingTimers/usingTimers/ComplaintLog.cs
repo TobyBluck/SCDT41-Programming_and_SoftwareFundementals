3159d92ac559dd4391914af27f7f26c45c9eeefd
TobyBluck <57405593+TobyBluck@users.noreply.github.com>
2020-08-18 17:46:23 +0100

    Uploading usingTimers example

---
+++ VisualStudio-Files/usingTimers/usingTimers/ComplaintLog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace usingTimers
+{
+    public class Complaint_Log
+    {
+
+        public string Title;
+        public string Name;
+        DateTime Submission;
+        public string Details;
+        public int Phone; 
+
+        public void LaunchComplaint()
+        {
+
+            Console.WriteLine("What is your name?");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("What is your contact number?");
+            Phone = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is the complaint?");
+            Console.ReadLine();
+
+            Submission = DateTime.Now; 
+
+        }
+
+    }
+}
